981706135cc666a4b1816c009bebba85d9bbe017
Loic Descotte <loic.descotte@kelkoo.com>
2019-09-20 15:07:12 +0200

    Initial commit

---
+++ src/main/scala/com/gitlab/kzs/zioworkshop/model/Stock.scala
@@ -0,0 +1,16 @@
+package com.gitlab.kzs.zioworkshop.model
+
+case class Stock(id:Int, value: Int)
+
+sealed abstract class StockError(cause: Throwable) extends Exception(cause)
+case object EmptyStock extends StockError(new Exception)
+case object StockNotFound extends StockError(new Exception)
+case class StockDBAccessError(cause: Throwable) extends StockError(cause)
+
+/**
+  * Stock business logic
+  */
+
+object Stock {
+  def validate(stock: Stock): Either[StockError, Stock] = ???
+}

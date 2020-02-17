4db1560414ee337dda636348ee22979e93692907
DescottL <Loic.Descotte-non-empl@moodys.com>
2020-02-17 16:45:35 +0100

    persistent db

--- src/main/scala/com/gitlab/kzs/zioworkshop/ExtServices.scala
+++ src/main/scala/com/gitlab/kzs/zioworkshop/ExtServices.scala
@@ -19,7 +19,7 @@ object ExtServicesLive extends ExtServices with Clock.Live  {
 
   val xa = Transactor.fromDriverManager[Task](
     "org.h2.Driver",
-    "jdbc:h2:mem:poc;INIT=RUNSCRIPT FROM 'src/main/resources/sql/create.sql'"
+    "jdbc:h2:file:./localdb;INIT=RUNSCRIPT FROM 'src/main/resources/sql/create.sql'"
     , "sa", ""
   )
   override val stockDao: StockDAO = new StockDAOLive(xa)
